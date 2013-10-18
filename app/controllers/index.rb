3948f14d27b9a845b838669be8d6e2624b1e8ac5
zachmokahn <zachmokahn@gmail.com>
2013-10-18 17:45:39 -0500

    sanititzed

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,4 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
@@ -9,8 +8,12 @@ end
 
 post '/sign_up' do
   @user = User.create(params[:user])
-  session[:user_id] = @user.id
-  redirect '/profile'
+  if @user.valid?
+    session[:user_id] = @user.id
+    redirect '/profile'
+  else
+    redirect '/'
+  end
 end
 
 get '/create_survey' do
@@ -18,21 +21,11 @@ get '/create_survey' do
 end
 
 post '/create_survey' do
-  @survey = Survey.create(params[:survey], user_id: session[:user_id])
-  @survey_question = Question.create(params[:question], survey_id: @survey.id)
-  @survey_response_1 = Response.create(content: params[:response][:content1], question_id: @survey_question.id )
-  @survey_response_2 = Response.create(content: params[:response][:content2], question_id: @survey_question.id )
-  @survey_response_3 = Response.create(content: params[:response][:content3], question_id: @survey_question.id )
-  @survey_response_4 = Response.create(content: params[:response][:content4], question_id: @survey_question.id )
+  # BETTER WAY TO CREATE SURVEY
   redirect '/profile'
 end
 
 get '/complete_survey/:id' do
   @survey = Survey.find(params[:id])
-    erb :complete_survey
+  erb :complete_survey
 end
-
-# post '/complete_survey/:id' do
-  
-#   redirect '/profile'
-# end
--- app/controllers/results-responses.rb
+++ app/controllers/results-responses.rb
@@ -1,5 +1,4 @@
 get '/surveyscreated/:survey_id' do
-  session[:user_id] = 2
   @user = User.find(session[:user_id])
   @survey = Survey.find(params[:survey_id])
   @questions = @survey.questions
@@ -8,7 +7,6 @@ get '/surveyscreated/:survey_id' do
 end
 
 get '/surveystaken/:survey_id' do
-  session[:user_id] = 2
   @user = User.find(session[:user_id])
   @survey = Survey.find(params[:survey_id])
   @questions = @survey.questions
