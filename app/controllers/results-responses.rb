1a0ae347b5a9f8c66230739b92fbc2003237fa67
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 14:18:14 -0500

    initial commit of results and responses

---
+++ app/controllers/results-responses.rb
@@ -0,0 +1,13 @@
+get '/surveyresults/:survey_id' do
+  @user = User.find(session[:user_id])
+  @survey = Survey.find(params[:survey_id])
+  erb :survey_results
+end
+
+get '/surveyresponse/:survey_id' do
+  @user = User.find(session[:user_id])
+  @survey = Survey.find(params[:survey_id])
+  @questions = @survey.questions
+  @user_responses = @user.responses.where(survey_id: @survey.id)
+  erb :survey_response
+end
