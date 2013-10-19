ac6ccbeaadf6f1241dc77666c1818c7209c13fa4
zachmokahn <zachmokahn@gmail.com>
2013-10-19 17:19:34 -0500

    Didn't actually change anything in application, adjusted survey_results to include a home link, fixed my end placement so that it didn't riderct until AFTER it creates all the user responses

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -24,14 +24,14 @@ post '/create_survey' do
   @survey = Survey.create(name: params[:survey_name], user_id: session[:user_id])
   params.each do |question, content_hash|
     if content_hash.class == Hash
-    content_hash.each do |key, value|
-      if key == "question"
-        @question = Question.create(survey_id: @survey.id, content: value)
-      else
-        Response.create(question_id: @question.id, survey_id: @survey.id, content: value)
+      content_hash.each do |key, value|
+        if key == "question"
+          @question = Question.create(survey_id: @survey.id, content: value)
+        else
+          Response.create(question_id: @question.id, survey_id: @survey.id, content: value)
+        end
       end
     end
-  end
   end
 
   redirect '/profile'
@@ -43,12 +43,13 @@ get '/complete_survey/:survey_id' do
 end
 
 post '/complete_survey/:survey_id' do
-  params[:q_r].each do |qr|
-    response = qr[1]
+  params[:q_r].each do |q, r|
+    response = r
     user_response = Userresponse.create(user_id: session[:user_id],
       response_id: response)
     completed_survey = Completedsurvey.create(user_id: session[:user_id],
       survey_id: params[:survey_id])
+  end
     redirect "/surveystaken/#{params[:survey_id]}"
   end
-end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {  
+$(document).ready(function() {
 
   var question_counter = 1;
   var response_counter = 1;
   function newResponse() { return '<div class="responses"><input type="text" name="question' + question_counter + '[response' + response_counter + ']" ><br></div><div class="ADD_RESPONSE_HERE"></div>' }
-  function newQuestion() { return '<div class="question"><label>Ask a Question: </label><input type="text" name="question' + question_counter + '[question]" ><br><br><label>Give some Choices: </label><div class="responses"><input type="text" name="question' + question_counter + '[response' + response_counter + ']" >  <input type="button" class="add_response" value="+"><br></div><div class="ADD_RESPONSE_HERE"></div><br><br></div>' }
+  function newQuestion() { return '<div class="question"><label>Ask a Question: </label><input type="text" name="question' + question_counter + '[question]" ><br><br><label>Give some Choices: </label><div class="responses"><input type="text" name="question' + question_counter + '[response' + response_counter + ']" >  <input type="button" class="add_response" value="+"><br></div><div class="ADD_RESPONSE_HERE"></div><br><br></div><div id="ADD_HERE"></div>' }
 
   $("#add_question").on("click", function(e)
   {
