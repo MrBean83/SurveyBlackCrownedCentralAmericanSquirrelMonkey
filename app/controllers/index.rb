26313a8c9cd91be996d794afea16983d110d230c
zachmokahn <zachmokahn@gmail.com>
2013-10-18 19:29:35 -0500

    collects responses

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -21,11 +21,23 @@ get '/create_survey' do
 end
 
 post '/create_survey' do
+
   # BETTER WAY TO CREATE SURVEY
   redirect '/profile'
 end
 
-get '/complete_survey/:id' do
-  @survey = Survey.find(params[:id])
+get '/complete_survey/:survey_id' do
+  @survey = Survey.find(params[:survey_id])
   erb :complete_survey
+end
+
+post '/complete_survey/:survey_id' do
+  params[:q_r].each do |qr|
+    response = qr[1]
+  user_response = Userresponse.create(user_id: session[:user_id],
+                                                              response_id: response)
+  completed_survey = Completedsurvey.create(user_id: session[:user_id],
+                                                                          survey_id: params[:survey_id])
+  redirect "/surveystaken/#{params[:survey_id]}"
+end
 end
