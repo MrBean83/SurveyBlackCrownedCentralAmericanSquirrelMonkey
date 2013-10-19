4f66e88ebb773b6c689333e502690599f2f62ba0
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-19 15:53:13 -0500

    got the crud almost working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -21,14 +21,14 @@ get '/create_survey' do
 end
 
 post '/create_survey' do
-  binding.pry
-  Survey.create(name: params[:survey_name], user_id: session[:user_id])
+  params.inspect
+  # Survey.create(name: params[:survey_name], user_id: session[:user_id])
   # BETTER WAY TO CREATE SURVEY
   # Create survey (user_id, name)
   # for each question
   # create the question (survey_id, content)
   # create the responses (question_id, survey_id, content)
-  redirect '/profile'
+  # redirect '/profile'
 end
 
 get '/complete_survey/:survey_id' do
@@ -39,10 +39,10 @@ end
 post '/complete_survey/:survey_id' do
   params[:q_r].each do |qr|
     response = qr[1]
-  user_response = Userresponse.create(user_id: session[:user_id],
-                                                              response_id: response)
-  completed_survey = Completedsurvey.create(user_id: session[:user_id],
-                                                                          survey_id: params[:survey_id])
-  redirect "/surveystaken/#{params[:survey_id]}"
-end
+    user_response = Userresponse.create(user_id: session[:user_id],
+      response_id: response)
+    completed_survey = Completedsurvey.create(user_id: session[:user_id],
+      survey_id: params[:survey_id])
+    redirect "/surveystaken/#{params[:survey_id]}"
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,23 +1,34 @@
-var question_counter = 2;
-var response_counter = 2;
+$(document).ready(function() {  
 
-$(document).ready(function() {
-  var newQuestion = '<div class="question"><label>Ask a Question: </label><input type="text" name="questions[' + question_counter + ']" ><br><br><label>Give some Choices: </label><div class="responses"><input type="text" name="response1" ><input type="button" class="add_response" value="+"><br><div class="ADD_RESPONSE_HERE"><br></div><br><br></div><div id="ADD_HERE"><br></div>';
-  var newResponse = '<div class="responses"><input type="text" name="response1" >  <input type="button" class="add_response" value="+"><br></div><div class="ADD_RESPONSE_HERE"></div>'
+  var question_counter = 1;
+  var response_counter = 1;
+  function newResponse() { return '<div class="responses"><input type="text" name="question' + question_counter + '[response' + response_counter + ']" ><br></div><div class="ADD_RESPONSE_HERE"></div>' }
+  function newQuestion() { return '<div class="question"><label>Ask a Question: </label><input type="text" name="question' + question_counter + '[question]" ><br><br><label>Give some Choices: </label><div class="responses"><input type="text" name="question' + question_counter + '[response' + response_counter + ']" >  <input type="button" class="add_response" value="+"><br></div><div class="ADD_RESPONSE_HERE"></div><br><br></div>' }
+
   $("#add_question").on("click", function(e)
   {
     e.preventDefault();
-    $("#ADD_HERE").replaceWith(newQuestion);
     question_counter++;
+    response_counter ++;
+    $("#ADD_HERE").replaceWith(newQuestion());
+
+    $(".add_response").last().on("click", function(event){
+      event.preventDefault();
+      response_counter ++;
+      ($(".ADD_RESPONSE_HERE").last()).replaceWith(newResponse());
+    });
   });
+
   $("#remove_question").on("click", function(e){
   // Look at .after as alternative to append
   $(".question").last().remove();
   question_counter--;
- });
-    $(".add_response").last().on("click", function(event){
-      event.preventDefault();
-     ($(".ADD_RESPONSE_HERE").last()).replaceWith(newResponse);
-    });
+});
+
+  $(".add_response").last().on("click", function(event){
+    event.preventDefault();
+    response_counter ++;
+    $(".ADD_RESPONSE_HERE").last().replaceWith(newResponse());
+  });
 });
 
