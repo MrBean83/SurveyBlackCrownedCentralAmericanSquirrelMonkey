39ba1abd1d1b8e061ee013f03c85db6b1a3754a1
Joe Levering <joseph.w.levering@gmail.com>
2013-10-20 20:02:47 -0500

    Added icons

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,11 @@
 class Photo < ActiveRecord::Base
   mount_uploader :filename, Uploader
   belongs_to :survey
+
+  def minify
+    self.filename.to_s.match(/(.*\/)([^\/]*$)/)
+    filename = $2.to_s
+    preface = $1.to_s
+    "#{preface}icon_#{filename}"
+  end
 end
